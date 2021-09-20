adf8b87b13e0b7cede2c3ebc653ddec17653abfe
Arthur <ajsantos5d@gmail.com>
2021-09-20 19:14:58 -0300

    final

--- Prova - 01/Prova01.js
+++ Prova - 01/Prova01.js
@@ -1,33 +1,29 @@
 function verificar() {
     vet_inputs = document.getElementsByTagName("input");
-    select = document.getElementsByTagName('select');
-
-    for (i=0; i<vet_inputs.length; i++){
-        if ((vet_inputs[i].type == "text") && (vet_inputs[i].value == "")) {
+    estadoCivil = document.getElementById("estCivil").value;
+    textArea = document.getElementById("textarea").value;
+    for (i = 0; i < vet_inputs.length; i++)
+        if ((vet_inputs[i].type == "text" && vet_inputs[i].value == "") || (vet_inputs[i].type == "email" && vet_inputs[i].value == "") || (vet_inputs[i].type == "tel" && vet_inputs[i].value == "") || (vet_inputs[i].type == "date" && vet_inputs[i].value == "")) 
+        {
             alert(`${vet_inputs[i].name} está vazio!!!`);
             break;
-        }
-        else if ((vet_inputs[i].type == "date") && (vet_inputs[i].value == "")) {
-            alert(`${vet_inputs[i].name} está vazio!!!`);
+        } 
+        else if (estadoCivil == "0" || estadoCivil == null) {
+            alert("Selecione o seu Estado civil!");
+            estadoCivil.focus();
+            break;
+        } 
+        else if (textArea == "" || textArea == null) {
+            alert("Conte-nos sobre voce esta vazio!!!");
+            textArea.focus();
+            break;
+        } 
+        else if (document.getElementById("sexoM").checked == false && document.getElementById("sexoF").checked == false) {
+            alert(`Sexo está vazio!`);
             break;
         }
-        else if ((vet_inputs[i].type == "email") && (vet_inputs[i].value == "")) {
-            alert(`${vet_inputs[i].name} está vazio!!!`);
+        else {
+            alert("Cadastro enviado com sucesso!!!");
             break;
         }
-        else if ((vet_inputs[i].type == "tel") && (vet_inputs[i].value == "")) {
-            alert(`${vet_inputs[i].name} está vazio!!!`);
-            break;
-        }
-        else if(document.getElementById('sexoM').checked == false && document.getElementById('sexoF').checked ==false) {  
-            alert(`Sexo está vazio!`)
-            break
-        }
-        for(j=0;j<select.length;j++){
-            if(select[i].value ==""){
-                alert(`Estado cívil está vazio!!!`);
-                break;
-            }
-        }
-    }
 }
