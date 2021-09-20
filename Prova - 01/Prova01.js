5d95b8029b22ecc15520789dd7af835ed09835b7
Arthur <ajsantos5d@gmail.com>
2021-09-20 14:23:00 -0300

    atividade avaliativa

--- Prova - 01/Prova01.js
+++ Prova - 01/Prova01.js
@@ -1,5 +1,7 @@
 function verificar() {
     vet_inputs = document.getElementsByTagName("input");
+    select = document.getElementsByTagName('select');
+
     for (i=0; i<vet_inputs.length; i++){
         if ((vet_inputs[i].type == "text") && (vet_inputs[i].value == "")) {
             alert(`${vet_inputs[i].name} está vazio!!!`);
@@ -17,9 +19,15 @@ function verificar() {
             alert(`${vet_inputs[i].name} está vazio!!!`);
             break;
         }
-        else if ((vet_inputs[i].type == "radio") && (vet_inputs[i].value == "")) {
-            alert(`${vet_inputs[i].name} está vazio!!!`);
-            break;
+        else if(document.getElementById('sexoM').checked == false && document.getElementById('sexoF').checked ==false) {  
+            alert(`Sexo está vazio!`)
+            break
+        }
+        for(j=0;j<select.length;j++){
+            if(select[i].value ==""){
+                alert(`Estado cívil está vazio!!!`);
+                break;
+            }
         }
     }
 }
